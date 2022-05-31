0138e52b79b8d0920f0a520952e5c4bdd11c67a3
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-05-31 14:15:13 -0300

    Estilização do arquivo MovieFile.css na parte da lista
    Estilização do arquivo MovieFile.css na parte da lista

--- src/components/MovieFile/MovieFile.jsx
+++ src/components/MovieFile/MovieFile.jsx
@@ -3,10 +3,10 @@ import '../MovieFile/MovieFile.css';
 
 export default ({ title, items }) => {
     return (
-        <div>
+        <div className="movieFile">
             <h2>{title}</h2>
             <div className="movieFile--listarea">
-                <div className='MovieFile--list'>
+                <div className='movieFile--list'>
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="movieFile--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
