627c7c19dd96e293b7abd5260e79b3a5d7448e7f
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-07 13:50:45 -0300

    criação do loading
    criação do loading.

--- src/App.js
+++ src/App.js
@@ -65,6 +65,12 @@ export default () => {
       <Footer />
 
 
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" width="650x650" alt="Carregando" />
+      </div>
+      }
+
     </div>
   );
 }
