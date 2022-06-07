4baf2b036ac8c8b2001cadaa909e7d1f64f60011
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-07 11:46:46 -0300

    Instalação dos pacotes npm do emotion react e emotion styled
    Instalação dos pacotes npm do emotion react e emotion styled.

--- src/components/MovieFile/MovieFile.jsx
+++ src/components/MovieFile/MovieFile.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import '../MovieFile/MovieFile.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+// import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+
 
 export default ({ title, items }) => {
     return (
         <div className="movieFile">
             <h2>{title}</h2>
+            <div className="movieFile--left">
+                <NavigateBeforeIcon style={{fontSize: 50}} />
+                
+            </div>
+
             <div className="movieFile--listarea">
                 <div className='movieFile--list'>
                     {items.results.length > 0 && items.results.map((item, key) => (
