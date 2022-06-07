a1f14bd907e6504cb7083035bdcdc46923f6a526
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-07 12:32:13 -0300

    Criada a função de rolagem das listas para esquerda
    Criada a função de rolagem das listas para esquerda.

--- src/components/MovieFile/MovieFile.jsx
+++ src/components/MovieFile/MovieFile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../MovieFile/MovieFile.css';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -6,18 +6,34 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 
 export default ({ title, items }) => {
+    const [scrollX, setScrollX] = useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + 150;
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+
+    }
+
     return (
         <div className="movieFile">
             <h2>{title}</h2>
-            <div className="movieFile--left">
+            <div className="movieFile--left" onClick={handleLeftArrow}>
                 <NavigateBeforeIcon style={{fontSize: 50}} />    
             </div>
-            <div className="movieFile--right">
+            <div className="movieFile--right" onClick={handleRightArrow}>
                 <NavigateNextIcon style={{fontSize: 50}} />
             </div>
 
             <div className="movieFile--listarea">
-                <div className='movieFile--list'>
+                <div className='movieFile--list' style={{ marginLeft: scrollX,
+                width: items.results.length * 150
+                }}>
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="movieFile--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
